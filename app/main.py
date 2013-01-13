2fdfb5023b857c8881c2efb8800fcff107fdab7d
Bryan Kendall <bryan@bryankendall.com>
2013-01-13 12:56:00 -0600

    Playing with simple web sockets

--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, boardgames")
+        self.render("index.html")
+
+class BinaryWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print "websocket opened"
+
+    def on_message(self, message):
+        print "received message: %s" % message
+        if message == "0": self.write_message("1")
+        elif message == "1": self.write_message("0")
+        else: self.write_message("-1")
+
+    def on_close(self):
+        print "websocket closed"
 
 application = tornado.web.Application([
     (r"/", MainHandler),
-])
+    (r"/ws/binary", BinaryWebSocket),
+], **{"static_path": os.path.join(os.path.abspath(os.path.dirname(__file__)), "static")})
 
 if __name__ == "__main__":
     application.listen(12000)
